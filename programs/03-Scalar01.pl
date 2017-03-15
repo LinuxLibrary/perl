074097ab5081b326619e8191a31b7215897cdc40
Arjun Shrinivas <vmsnivas@gmail.com>
2017-03-15 06:21:35 +0000

    Added programs/03-Scalar01.pl

---
+++ programs/03-Scalar01.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -w
+# Author : Arjun Shrinivas
+# Date: 15th MAR 2017
+
+$FIRSTNAME = "Arjun";
+$LASTNAME = "Shrinivas";
+$AGE = 30;
+$EXACT = "30.618";
+print "$FIRSTNAME $LASTNAME age is $AGE exact is $EXACT\n";
+
+# END

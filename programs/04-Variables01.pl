5d1863e58eac477ddc9b395d7bf8155e2c4183ca
Arjun Shrinivas <vmsnivas@gmail.com>
2017-03-15 07:03:51 +0000

    Modified 04-Variables01.pl

--- programs/04-Variables01.pl
+++ programs/04-Variables01.pl
@@ -6,10 +6,11 @@ use strict;
 
 # Global and Lexical Variables
 my $FIRSTNAME = "Arjun";
-my $LASTNAME = "Shrinivas";
 my $AGE = 30;
 our $COUNT = 1; # Global Variable
 
+	print "Please input your lastname: ";
+	my $LASTNAME = <STDIN>; # Assigning variable through STD Input
 	print "$FIRSTNAME $LASTNAME is", "$AGE", "\n";
 	print "Count is $COUNT\n";
 
@@ -19,7 +20,7 @@ my $FIRSTNAME = "LinuxLibrary";
 my $AGE = 4;
 
 	print "Firstname in block is: $FIRSTNAME\n";
-	print "$FIRSTNAME's age is $AGE\n";
+	print "$FIRSTNAME","'s age is $AGE\n";
 	print "Count is $COUNT\n";
 }
 

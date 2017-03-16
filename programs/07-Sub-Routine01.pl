27c073c3c73d059dffcfab4a0ffbdf4efdf432aa
Arjun Shrinivas <vmsnivas@gmail.com>
2017-03-16 08:13:29 +0000

    Added 07-Sub-Routine01.pl

---
+++ programs/07-Sub-Routine01.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+# Author: Arjun Shrinivas
+# Date: 16th Mar 2017
+
+use strict;
+
+sub MySub1 {
+	my $NAME = "LinuxLibrary";
+	print "$NAME\n";
+}
+
+sub MySub2 {
+	my $FIRSTNAME = @_[0];
+	my $LASTNAME = @_[1];
+	print "Hello $FIRSTNAME $LASTNAME\n";
+}
+
+MySub1();
+MySub2("Arjun","Shrinivas");
+
+# END

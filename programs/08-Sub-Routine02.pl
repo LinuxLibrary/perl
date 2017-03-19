8418d8d352b673b85d04f1171def86c8315d9fee
Arjun Shrinivas <vmsnivas@gmail.com>
2017-03-19 07:24:31 +0000

    Modified 08-Sub-Routine02.pl

--- programs/08-Sub-Routine02.pl
+++ programs/08-Sub-Routine02.pl
@@ -2,6 +2,22 @@
 # Author : Arjun Shrinivas
 # Date : 18th Mar 2017
 
+use warnings;
+use strict;
+
+open (Han1, ">> PerlLog.txt") || die "Errors opening file: $!";
+my $ETCDIR = `ls -l /etc`;
+my $MESSAGE = "Launching 08-Sub-Routine02.pl\n";
+
+log_message("$MESSAGE");
+log_message("$ETCDIR");
+
+
+sub log_message {
+	my $TIME = localtime;
+	print "$TIME - $_[0]", "\n";
+	print Han1 "$TIME - $_[0]", "\n";
+}
 
 
 # END

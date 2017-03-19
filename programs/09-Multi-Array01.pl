5a977a386e6d6bb4e3e25b3995a6c2cf20dc5c85
Arjun Shrinivas <vmsnivas@gmail.com>
2017-03-19 11:33:26 +0000

    Added ../programs/09-Multi-Array01.pl

---
+++ programs/09-Multi-Array01.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+# Author : Arjun Shrinivas
+# Date : 19th Mar 2017
+
+@ARRAY1 = (["1","2"],["3","4"],["5","6"]);
+
+print "@ARRAY1", "\n";
+print "@ARRAY1[0..1]", "\n";
+print "@ARRAY1[0]", "\n";
+
+print "$ARRAY1[0][0]", "\n";
+print "$ARRAY1[0][1]", "\n";
+print "$ARRAY1[1][0]", "\n";
+print "$ARRAY1[1][1]", "\n";
+print "$ARRAY1[2][0]", "\n";
+print "$ARRAY1[2][1]", "\n";
+
+# END

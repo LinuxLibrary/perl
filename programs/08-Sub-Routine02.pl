01ef21d17cdbcd5edd7891bc00ae748e8a827b52
Arjun Shrinivas <vmsnivas@gmail.com>
2017-03-20 04:02:20 +0000

    Modified 08-Sub-Routine02.pl

--- programs/08-Sub-Routine02.pl
+++ programs/08-Sub-Routine02.pl
@@ -7,12 +7,15 @@ use strict;
 
 open (Han1, "> output/PerlLog.txt") || die "Errors opening file: $!";
 my $ETCDIR = `ls -l /etc | grep '^d.*rc'`;
-chomp $ETCDIR;
+my $ETCDIR2 = system("ls -l /etc | grep '^d.*rc'");
+chomp ($ETCDIR, $ETCDIR2);
 my $MESSAGE = "Launching 08-Sub-Routine02.pl\n";
 chomp $MESSAGE;
 
-log_message("$MESSAGE");
+log_message("$MESSAGE - 1");
 log_message("$ETCDIR");
+log_message("$MESSAGE - 2");
+log_message("$ETCDIR2");
 
 
 sub log_message {
